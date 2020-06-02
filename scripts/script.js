6af9c31cdb46676742ea2abb4a88892c38ac011b
szymonszoldra <szymon.szoldra@gmail.com>
2020-06-02 13:50:27 +0200

    add favicon and aboutme

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,14 @@
 particlesJS.load('particles-js', '/scripts/particlesjs.json', function () {
   console.log('callback - particles.js config loaded');
 });
+
+particlesJS.load('particles-js2', '/scripts/particlesjs2.json', function () {
+  console.log('callback - particles2.js config loaded');
+});
+
+const arrow = document.querySelector('.header__arrow');
+const technologies = document.querySelector('.technologies');
+
+arrow.addEventListener('click', () => technologies.scrollIntoView({
+  behavior: "smooth"
+}));
